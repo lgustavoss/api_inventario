058977897357579049aedd00ac5451af033fa34f
Luis Gustavo <luis_gustavope@icloud.com>
2024-02-18 15:35:16 -0300

    - Atualizado comentários dos endpoints

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,12 @@ from .views import (
 )
 
 urlpatterns = [
+    # Grafico que mostra a quantidade de empresas cadastradas por mês nos ultimos 12 meses
     path('grafico_empresas_1/', EmpresasCadastradasNosUltimos12Meses.as_view(), name='empresas-cadastradas-ultimos-12-meses'),
+
+    # Grafico que mostra a quantidade de colaboradores cadastrados por mês nos ultimos 12 meses
     path('grafico_colaboradores_1/', ColaboradoresCadastradosNosUltimos12Meses.as_view(), name='colaboradores-cadastrados-ultimos-12-meses'),
+
+    # Grafico que mostra a quantidade de equipamentos cadastrados por mês nos ultimos 12 meses
     path('grafico_equipamentos_1/', EquipamentosCadastradosNosUltimos12Meses.as_view(), name='equipamentos-cadastrados-ultimos-12-meses'),
 ]
